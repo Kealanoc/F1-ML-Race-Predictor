7eeb631862da29a6e69062658581238af2ed67c0
Kealan O'Connor <kealan.3oconnor@gmail.com>
2021-01-18 17:14:03 +0000

    Fixed quali data and added weather data

--- code/Collection/Dataframe_QualiData.py
+++ code/Collection/Dataframe_QualiData.py
@@ -7,33 +7,31 @@ import time
 start_time = time.time()
 
 qualifying_results = pd.DataFrame()
-for year in list(range(1983,2020)):
-    url = 'https://www.formula1.com/en/results.html/{}/races.html'
+for year in list(range(1983,2021)):
+    url = "https://www.formula1.com/en/results.html/{}/races.html"
     r = requests.get(url.format(year))
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, "html.parser")
     
-    year_links = []
-    for page in soup.find_all('a', attrs = {'class':"resultsarchive-filter-item-link FilterTrigger"}):
+    links = []
+    for page in soup.find_all("a", attrs = {'class':"resultsarchive-filter-item-link FilterTrigger"}):
         link = page.get('href')
-        if f'/en/results.html/{year}/races/' in link: 
-            year_links.append(link)
+        if f"/en/results.html/{year}/races/" in link: 
+            links.append(link)
 
     year_df = pd.DataFrame()
-    new_url = 'https://www.formula1.com{}'
-    for n, link in list(enumerate(year_links)):
-        link = link.replace('race-result.html', 'starting-grid.html')
-        df = pd.read_html(new_url.format(link))
-        df = df[0]
-        df['season'] = year
-        df['round'] = n+1
-        for col in df:
-            if 'Unnamed' in col:
-                df.drop(col, axis = 1, inplace = True)
+    new_url = "https://www.formula1.com{}"
+    for i, link in list(enumerate(links)):
+        link = link.replace("race-result.html", "starting-grid.html")
+        dataframe = pd.read_html(new_url.format(link))
+        dataframe = dataframe[0]
+        dataframe["season"] = year
+        dataframe["round"] = i+1
+        for column in dataframe:
+            if "Unnamed" in column:
+                dataframe.drop(column, axis=1, inplace=True)
 
-        year_df = pd.concat([year_df, df])
+        year_df = pd.concat([year_df, dataframe])
 
     qualifying_results = pd.concat([qualifying_results, year_df])
-qualifying_results.to_csv("../Data/QualifyingResults.csv", index = False)
+qualifying_results.to_csv("../Data/QualifyingResults.csv", index=False)
 print("--- %s seconds ---" % (time.time() - start_time))
-
-
---
+++ code/Collection/Dataframe_Weather.py
@@ -0,0 +1,50 @@
+import bs4
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import requests
+import time
+start_time = time.time()
+
+race = pd.read_csv("../Data/RaceData.csv")
+weather = race.iloc[:,[0,1,2]]
+data = []
+
+for links in race.URL:
+    try:
+        print(links)
+        dataframe = pd.read_html(links)[0]
+        if "Weather" in list(dataframe.iloc[:,0]):
+            i = list(dataframe.iloc[:,0]).index("Weather")
+            data.append(dataframe.iloc[i,1])
+        else:
+            dataframe = pd.read_html(links)[1]
+            if "Weather" in list(ds.iloc[:,0]):
+                i = list(df.iloc[:,0]).index("Weather")
+                data.append(df.iloc[i,1])
+            else:
+                dataframe = pd.read_html(links)[2]
+                if "Weather" in list(ds.iloc[:,0]):
+                    i = list(dataframe.iloc[:,0]).index("Weather")
+                    data.append(dataframe.iloc[i,1])
+                else:
+                    dataframe = pd.read_html(links)[3]
+                    if "Weather" in list(ds.iloc[:,0]):
+                        i = list(dataframe.iloc[:,0]).index("Weather")
+                        data.append(dataframe.iloc[i,1])
+    except:
+        data.append("N/A")
+
+weather['weather'] = data
+weather_class = {"weather_warm": ['clear', 'warm', 'hot', 'sunny', 'fine', 'mild'],
+               "weather_cold": ['cold', 'fresh', 'chilly', 'cool'],
+               "weather_dry": ['dry'],
+               "weather_wet": ['showers', 'wet', 'rain', 'damp', 'thunderstorms', 'rainy'],
+               "weather_cloudy": ['overcast', 'clouds', 'cloudy', 'grey']}
+
+weather_data = pd.DataFrame(columns= weather_class.keys())
+for column in weather_data:
+    weather_data[column] = weather["weather"].map(lambda x: i if any(i in weather_class[column] for i in x.lower().split()) else 0)
+weather_data = pd.concat([weather, weather_data], axis=1)
+weather_data.to_csv("../Data/Weather.csv", index=False)
+print("--- %s seconds ---" % (time.time() - start_time))
