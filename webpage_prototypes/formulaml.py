dd3dc3a2badc9fa19a81475e8d7db66737e605c1
Peter Browne <peter.browne26@mail.dcu.ie>
2021-02-08 18:37:35 +0000

    Updates to webpage

--- webpage_prototypes/formulaml.py
+++ webpage_prototypes/formulaml.py
@@ -6,8 +6,24 @@ import json, csv, pandas
 
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
+
+@app.route('/driver')
+def getjson():
+    
+    data = []
+
+    with open('DriverStandings.csv') as csvFile:
+        csvReader = csv.DictReader(csvFile)
+        for rows in csvReader:
+            data.append(rows)
+    
+    with open('DriverStandings.json', 'w') as jsonFile:
+        jsonFile.write(json.dumps(data, indent=4))
+
+    return jsonify(data)
+
+@app.route('/driver')
+def getapi():
     return render_template('driver.html')
 
 if __name__ == "__main__":
