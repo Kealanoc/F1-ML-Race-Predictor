d73769b17d74dace7bb58b2b863f8956c80ff061
peterbrowne <winneris1peterbrowne@gmail.com>
2021-03-06 20:33:25 +0000

    Navbar fix

--- code/webpages/dash/index.py
+++ code/webpages/dash/index.py
@@ -8,7 +8,7 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-from formulaml_dash import app
+from formulaml_dash import app, server
 from template import verstappen, albon, ricciardo, ocon, stroll, perez, russell, latifi, magnussen, grosjean, norris, sainz, leclerc, vettel, bottas, hamilton, raikkonen, giovinazzi, gasly, kvyat
 from Predictor import Predictor, Predictor_Info
 from teams_template import red_bull, renault, mercedes, ferrari, mclaren, racingpoint, alphatauri, alfaromeo, haas, williams
@@ -22,71 +22,71 @@ app.layout = html.Div([
 @app.callback(Output('page-content','children', ),
               Input('url', 'pathname', ))
 def display_page(pathname):
-    if pathname == '/driver/verstappen':
+    if pathname == '/driver/verstappen/':
         return verstappen.layout
-    elif pathname == '/driver/albon':
+    elif pathname == '/driver/albon/':
         return albon.layout
-    elif pathname == '/driver/ricciardo':
+    elif pathname == '/driver/ricciardo/':
         return ricciardo.layout
-    elif pathname == '/driver/ocon':
+    elif pathname == '/driver/ocon/':
         return ocon.layout
-    elif pathname == '/driver/stroll':
+    elif pathname == '/driver/stroll/':
         return stroll.layout
-    elif pathname == '/driver/perez':
+    elif pathname == '/driver/perez/':
         return perez.layout
-    elif pathname == '/driver/russell':
+    elif pathname == '/driver/russell/':
         return russell.layout
-    elif pathname == '/driver/latifi':
+    elif pathname == '/driver/latifi/':
         return latifi.layout
-    elif pathname == '/driver/magnussen':
+    elif pathname == '/driver/magnussen/':
         return magnussen.layout
-    elif pathname == '/driver/grosjean':
+    elif pathname == '/driver/grosjean/':
         return grosjean.layout
-    elif pathname == '/driver/norris':
+    elif pathname == '/driver/norris/':
         return norris.layout
-    elif pathname == '/driver/sainz':
+    elif pathname == '/driver/sainz/':
         return sainz.layout
-    elif pathname == '/driver/leclerc':
+    elif pathname == '/driver/leclerc/':
         return leclerc.layout
-    elif pathname == '/driver/vettel':
+    elif pathname == '/driver/vettel/':
         return vettel.layout
-    elif pathname == '/driver/bottas':
+    elif pathname == '/driver/bottas/':
         return bottas.layout
-    elif pathname == '/driver/hamilton':
+    elif pathname == '/driver/hamilton/':
         return hamilton.layout
-    elif pathname == '/driver/raikkonen':
+    elif pathname == '/driver/raikkonen/':
         return raikkonen.layout
-    elif pathname == '/driver/giovinazzi':
+    elif pathname == '/driver/giovinazzi/':
         return giovinazzi.layout
-    elif pathname == '/driver/gasly':
+    elif pathname == '/driver/gasly/':
         return gasly.layout
-    elif pathname == '/driver/kvyat':
+    elif pathname == '/driver/kvyat/':
         return kvyat.layout
-    elif pathname == '/team/red_bull':
+    elif pathname == '/team/red_bull/':
         return red_bull.layout
-    elif pathname == '/team/renault':
+    elif pathname == '/team/renault/':
         return renault.layout
-    elif pathname == '/team/mercedes':
+    elif pathname == '/team/mercedes/':
         return mercedes.layout
-    elif pathname == '/team/ferrari':
+    elif pathname == '/team/ferrari/':
         return ferrari.layout
-    elif pathname == '/team/mclaren':
+    elif pathname == '/team/mclaren/':
         return mclaren.layout
-    elif pathname == '/team/racingpoint':
+    elif pathname == '/team/racingpoint/':
         return racingpoint.layout
-    elif pathname == '/team/alphatauri':
+    elif pathname == '/team/alphatauri/':
         return alphatauri.layout
-    elif pathname == '/team/alfaromeo':
+    elif pathname == '/team/alfaromeo/':
         return alfaromeo.layout
-    elif pathname == '/team/haas':
+    elif pathname == '/team/haas/':
         return haas.layout
-    elif pathname == '/team/williams':
+    elif pathname == '/team/williams/':
         return williams.layout
-    elif pathname == '/predictor':
+    elif pathname == '/predictor/':
         return Predictor.layout
-    elif pathname == '/predictor_info':
+    elif pathname == '/predictor_info/':
         return Predictor_Info.layout
-    elif pathname == '/home':
+    elif pathname == '/':
         return season.layout
     else:
         return dbc.Jumbotron(
